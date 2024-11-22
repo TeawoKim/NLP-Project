27703c7a3a186002ca2c12cde00928edb0d64219
TeawoKim <tkim52@my.centennialcollege.ca>
2024-11-22 15:43:58 -0500

    data loading

--- NLP Project/data_loader.py
+++ NLP Project/data_loader.py
@@ -1,3 +1,6 @@
 import pandas as pd
 
-data = pd.read_csv("")
+def load_data(file_path):
+    data = pd.read_csv(file_path)[["CONTENT", "CLASS"]]
+    data.columns = ["Comment", "Label"]
+    return data
---
+++ NLP Project/main.py
@@ -0,0 +1,16 @@
+from data_loader import load_data
+
+if __name__ == "__main__":
+
+    data_files = [
+        "data/Youtube01-Psy.csv",
+        "data/Youtube01-KatyPerry.csv",
+        "data/Youtube01-LMFAO.csv",
+        "data/Youtube01-Eminem.csv",
+        "data/Youtube01-Shakria.csv",
+        ]
+
+    for file_path in data_files:
+        print(f"Processing file: {file_path}")
+        data = load_data(file_path)
+       
